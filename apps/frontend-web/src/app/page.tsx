82cc73e7563b52a6a003c2988a86f55131ef51bd
phongnttt <111858261+phongnttt@users.noreply.github.com>
2023-03-23 17:24:02 +0700

    add format wrong

--- apps/frontend-web/src/app/page.tsx
+++ apps/frontend-web/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home(props: any) {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="title_subtitle">Hello World Nhớ</div>
+             <div
+             
+             
+             className="title_subtitle">Hello World Nhớ</div>
     </div>
   );
 }
